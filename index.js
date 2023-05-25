dc905e9b6ad698b47629b0f315aaff476f85cbb5
student <swanjikumaina@gmail.com>
2023-05-25 20:17:21 +0300

    main

---
+++ index.js
@@ -0,0 +1,20 @@
+// Change the background color to silver
+document.body.style.backgroundColor = "silver";
+// Change the font color of the h1 title tag to green
+let heading1 = document.getElementById("title");
+heading1.style.color = "green";
+// Change the font case of the h3 title tags to uppercase
+let heading3 = document.getElementsId("h3");
+
+// let title = heading3.toUpperCase()
+
+
+
+// Add one more fruit to the fruits list
+let fruitsList = document.getElementById("fruList");
+fruitsList.append("Pasion");
+// Add one more vegetable to the vegetables list
+let vegetablesList = document.getElementById("vegList");
+
+// vegetablesList.setAttribute("id","li")
+vegetablesList.append("cabbage");
